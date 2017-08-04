f54c17066a8df3b2408fd96f679f742ee884acab
RPmarvin <30728701+RPmarvin@users.noreply.github.com>
2017-08-04 10:26:53 -0500

    v 1.0

---
+++ Zisary/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zisary.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Models.TiendaEntities bd = new Models.TiendaEntities();
+        // GET: HomeD:\e-commer\proyectoZ\ComercioElectronico\Zisary\Views\Home\
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Buscar(String id=" ")
+        {
+            //logica de acceso a base de datos
+            var productos = bd.Producto.Where(x=>x.Tag.Contains(id)).Take(20).ToList();
+            ViewBag.clave = id;
+            //lista de productos
+            return View(productos);
+        }
+    }
+}
