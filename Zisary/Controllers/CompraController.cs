df4c4920767d27a0a7f4a775cc2889ca228f3c22
RPmarvin <30728701+RPmarvin@users.noreply.github.com>
2017-08-05 06:34:08 -0500

    cambios

---
+++ Zisary/Controllers/CompraController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zisary.Controllers
+{
+    public class CompraController : Controller
+    {
+        // GET: Compra
+        public ActionResult Paso1()
+        {
+            return View();
+        }
+        public ActionResult Paso2()
+        {
+            return View();
+        }
+        public ActionResult Paso3()
+        {
+            return View();
+        }
+        public ActionResult Paso4()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult RealizarPedido(List<Pedido> p){
+            //guardar en base de datos
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+        public class Pedido
+        {
+            public int ProductoId { get; set; }
+            public string Denominacion { get; set; }
+            public int Cantidad { get; set; }
+            public string Imagen { get; set; }
+            public decimal Precio { get; set; }
+        }
+
+    }
+}
